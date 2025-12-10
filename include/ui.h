74cb34354fef234562a08f46b3abe6837e8b086c
Oumar92140 <oumar.barry09@icloud.com>
2025-12-10 15:52:42 +0100

    Update ui.h

--- include/ui.h
+++ include/ui.h
@@ -2,7 +2,13 @@
 #define UI_H
 
 #include "struct.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
-void showMenu(entity *player);
+int initUI(SDL_Window **window, SDL_Renderer **renderer);
+
+void showMenu(entity *player, SDL_Renderer *renderer);
+
+void cleanupUI(SDL_Window *window, SDL_Renderer *renderer);
 
 #endif
