f1c5ee89163ffd3b852e170fa70127ef425d9581
Oumar92140 <oumar.barry09@icloud.com>
2025-12-10 09:18:53 +0100

    Update interface.h

--- include/interface.h
+++ include/interface.h
@@ -1,9 +1,7 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-// Point d'entrée unique de l'interface du jeu (UI SDL)
-// Lance la boucle du jeu (menu, sélection de classe, combat, highscores, etc.).
-// Retourne 0 en cas de succès, >0 en cas d'erreur d'initialisation.
+
 int run_game(void);
 
 #endif
